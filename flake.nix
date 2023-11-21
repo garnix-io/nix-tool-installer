d2e308bb74636df615d006fb417f0f382b69e0df
Alex David <alexdavid@users.noreply.github.com>
2023-11-20 18:54:04 -0800

    Use qemu over vagrant for testing (#6)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/23.05";
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in
       rec {
         # { toolName: string, baseUrl: string, flakeLocation: string, testCommand: string } -> derivation
@@ -73,7 +73,7 @@
         packages = {
           exampleInstallerFiles = self.lib.${system}.mkInstallScriptFiles {
             toolName = "hello";
-            baseUrl = "http://192.168.56.1:8000";
+            baseUrl = "http://10.0.2.2:8000";
             allowHttp = true;
             flakeLocation = "github:nixos/nixpkgs/f098c1634d7bc427d9fe51e5f536c8aed65c1991#hello";
             testCommand = "hello";
@@ -88,6 +88,74 @@
                 exec ${pkgs.python3}/bin/python3 -m http.server 2> /dev/null
               '');
           };
+          bootVm = {
+            type = "app";
+            program =
+              let
+                ubuntu = pkgs.fetchurl {
+                  url = "https://cloud-images.ubuntu.com/jammy/20231027/jammy-server-cloudimg-amd64.img";
+                  hash = "sha256-a7Ukf4eRm4A8IRr9GvdLMJa+boNNrCnPrHEdrXLq/qg=";
+                };
+                # See https://cloudinit.readthedocs.io/en/latest/reference/index.html
+                cloudcfg = {
+                  ssh_pwauth = true;
+                  users = [{
+                    name = "test";
+                    plain_text_passwd = "test";
+                    lock_passwd = false;
+                    sudo = "ALL=(ALL) ALL";
+                    shell = "/bin/bash";
+                  }];
+                  runcmd = [
+                    # Attempt to briefly connect to port 2223 on the host to
+                    # notify the boot-vm script that we have fully booted and
+                    # it can stop waiting.
+                    [ "nc" "-vz" "10.0.2.2" "2223" ]
+                  ];
+                };
+                cloud-init-img = pkgs.runCommand "user-data-img" { }
+                  "${pkgs.cloud-utils}/bin/cloud-localds $out ${pkgs.writeTextFile {
+                    name = "userdata";
+                    text = "#cloud-config\n" + builtins.toJSON cloudcfg;
+                  }}";
+              in
+              builtins.toString (pkgs.writeScript "boot-vm" ''
+                set -eux
+
+                ROOT_IMG=$(mktemp)
+                CLOUD_INIT_IMG=$(mktemp)
+                ${pkgs.qemu}/bin/qemu-img create -o backing_file=${ubuntu},backing_fmt=qcow2 -f qcow2 $ROOT_IMG 3G
+                cp ${cloud-init-img} $CLOUD_INIT_IMG
+                chmod +w $CLOUD_INIT_IMG
+                ${pkgs.qemu}/bin/qemu-system-x86_64 \
+                  -m 1G \
+                  -enable-kvm \
+                  -machine q35 \
+                  -device intel-iommu \
+                  -drive file=$ROOT_IMG,format=qcow2 \
+                  -drive file=$CLOUD_INIT_IMG,format=raw \
+                  -nographic \
+                  -device e1000,netdev=net0 \
+                  -netdev user,id=net0,hostfwd=tcp::2222-:22 \
+                  &
+                # Wait for the runcmd to call us back to let us know that we are fully booted.
+                # (See runcmd in the flake file under cloudcfg)
+                nc -vl 2223
+              '');
+          };
+          sshVm = {
+            type = "app";
+            program = builtins.toString (pkgs.writeScript "ssh-vm" ''
+              ${pkgs.sshpass}/bin/sshpass -p test ssh \
+                -t \
+                -o "StrictHostKeyChecking no" \
+                -o "UserKnownHostsFile=/dev/null" \
+                localhost \
+                -p 2222 \
+                -l test \
+                "$@"
+            '');
+          };
         };
         checks = {
           shellCheck = pkgs.runCommand "shellcheck"
@@ -100,8 +168,9 @@
         devShells = {
           default = pkgs.mkShell {
             buildInputs = [
+              pkgs.just
+              pkgs.lsof
               pkgs.shellcheck
-              pkgs.vagrant
             ];
           };
         };
