e84ae25f4d11e10aac911332f995cb2ca940bda6
Jean-François Roche <jfroche@pyxel.be>
2023-12-21 16:41:54 +0100

    Upgrade to the latest nixpkgs stable (#13)

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Install scripts for nix-based tools";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/23.05";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/23.11";
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in
